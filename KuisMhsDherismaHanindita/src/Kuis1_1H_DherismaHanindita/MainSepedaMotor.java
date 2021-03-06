bc65a17df585c602a83c4eae82fb3a1b7b0866b5
Dherisma Hanindita <dherismadedek@gmail.com>
2021-03-06 08:52:24 +0700

    Quis1

---
+++ KuisMhsDherismaHanindita/src/Kuis1_1H_DherismaHanindita/MainSepedaMotor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kuis1_1H_DherismaHanindita;
+
+/**
+ *
+ * @author Dherisma
+//Nama    : Dherisma Hanindita Utami
+//NIM/ No : 2041720018
+//Kelas   : TI 1H
+ */
+import java.util.Scanner;
+public class MainSepedaMotor {
+    Scanner sc = new Scanner(System.in);
+    SepedaMotor[]spdmtr = new SepedaMotor[4];
+   
+    static void pemilihan(){
+        System.out.println(" ___________________________");
+        System.out.println("|     DAFTAR PEMILIHAN      |");
+        System.out.println("|___________________________|");
+        SepedaMotor[]spd = new SepedaMotor[4];
+        SepedaMotor[]spdmtr = new SepedaMotor[4];
+        spdmtr[0]= new SepedaMotor("~~~~HONDA~~~~");
+        spdmtr[1]= new SepedaMotor("~~~~YAMAHA~~~~");
+        spdmtr[2]= new SepedaMotor("~~~~DUCATI~~~~");
+        spdmtr[3]= new SepedaMotor("~~~~SUZUKI~~~~");
+    
+        
+}
--- KuisMhsDherismaHanindita/src/Kuis1_1H_DherismaHanindita/SepedaMotor.java
+++ KuisMhsDherismaHanindita/src/Kuis1_1H_DherismaHanindita/SepedaMotor.java
@@ -7,8 +7,36 @@ package Kuis1_1H_DherismaHanindita;
 
 /**
  *
- * @author Holla
+ * @author Dherisma
+//Nama    : Dherisma Hanindita Utami
+//NIM/ No : 2041720018/09
+//Kelas   : TI 1H
  */
+import java.util.Scanner;
 public class SepedaMotor {
+    String merk;
+    boolean kontakMotor;
+    int kecepatanMotor;
+
+    SepedaMotor(String honda, double d, int i) {
+        }
+    
+    void SepedaMotor(String merk, boolean kontakMotor, int kecepatanMotor){
+        this.merk = merk;
+        this.kontakMotor = kontakMotor;
+        this.kecepatanMotor = kecepatanMotor;
+    }
+    static void pemilihan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" ___________________________");
+        System.out.println("|     DAFTAR PEMILIHAN      |");
+        System.out.println("|___________________________|");
+        SepedaMotor game1 = new SepedaMotor("Honda", 2.33, 22);
+        VideoGame game2 = new VideoGame(2, 41000, "Adventure");
+        VideoGame game3 = new VideoGame(3, 36000, "Real Time Strategi");
+        
+        game1.pilihan();
+        game2.pilihan();
+        game3.pilihan();
     
 }
